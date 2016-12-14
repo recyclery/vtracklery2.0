8ede31fed960600697e2383584f90f4a1c6396f2
mmazanec22 <melanie.mazanec@gmail.com>
2016-12-14 16:25:59 -0600

    Start wworking on master... oops

--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -3,6 +3,7 @@ class WorkTime < ApplicationRecord
   belongs_to :event
   belongs_to :work_type
 
+# before save maybe? enforce uniqueness-- throw error if there is another shift that overlaps
 
   def work_start_string
   end
