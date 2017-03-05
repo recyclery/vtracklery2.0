9854e973e50ecbb9a0f945ccd4dab4d66b79bc28
mmazanec22 <melanie.mazanec@gmail.com>
2017-03-05 00:46:23 -0600

    Start to build admin feature

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
   end
 
   devise_scope :worker do
-    root 'workers#show'
+    root 'layouts#index'
   end
+
+  get "workers" => "workers#index", as: "workers"
+
+  get "workers/:id" => "workers#show", as: "worker_show"
+
 
   post "workers/:id/work_times" => "work_times#create", as: "create_work_time"
 
--- db/migrate/20161101211118_create_user_roles.rb
+++ db/migrate/20161101211118_create_user_roles.rb
@@ -2,7 +2,6 @@ class CreateUserRoles < ActiveRecord::Migration[5.0]
   def change
     create_table :user_roles do |t|
       t.string :role
-
       t.timestamps
     end
   end
